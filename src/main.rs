3531fcca91923201d45c100a37d0066dbd5d699f
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2025-08-16 16:52:11 +0200

    update default signtool path

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ struct Args {
     #[arg(
         long,
         env = "SIGNTOOL_PATH",
-        default_value = r"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe"
+        default_value = r"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe"
     )]
     sign_tool_path: String,
 
